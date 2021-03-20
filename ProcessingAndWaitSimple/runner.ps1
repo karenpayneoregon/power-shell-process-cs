e5ff1692b1610717edbed70ecfb77feb833e06bb
Karen Payne <Karen.1.Payne@oregon.gov>
2021-03-19 17:43:51 -0700

    added comments

--- ProcessingAndWaitSimple/runner.ps1
+++ ProcessingAndWaitSimple/runner.ps1
@@ -1,1 +1,9 @@
-﻿reg export Hkey_local_Machine\Software\Microsoft\ASP.NET C:\OED\Dotnetland\VS2019\PowerShellSolution\ProcessingAndWaitSimple\bin\Debug\net5.0-windows\regFile.txt
+﻿<#
+.SYNOPSIS
+    Get details from the registry
+.DESCRIPTION
+    For code in PowerShellOperations1.cs
+.NOTES
+    Prerequisite   : Tested with PowerShell V5.1
+#>
+reg export Hkey_local_Machine\Software\Microsoft\ASP.NET C:\OED\Dotnetland\VS2019\PowerShellSolution\ProcessingAndWaitSimple\bin\Debug\net5.0-windows\regFile.txt
